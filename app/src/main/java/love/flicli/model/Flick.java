e23e0323ef00118f64eb497f0a31b75f47e0db09
tommi.bonetti <tommi.bonetti@gmail.com>
2017-05-19 21:20:50 +0200

    Add Immutable class to manadge photos and comments

---
+++ app/src/main/java/love/flicli/model/Flick.java
@@ -0,0 +1,41 @@
+package love.flicli.model;
+
+import net.jcip.annotations.Immutable;
+
+/**
+ * Created by tommaso on 19/05/17.
+ */
+
+/*
+ * This class represent every single photo.
+ * When we make a request and store the results, we managed the photo in this format.
+ */
+
+@Immutable
+public class Flick {
+
+    private String image_thb = "";
+    private String id = "";
+    private String author = "";
+    private String imageUrl = "";
+    private String title = "";
+
+    public Flick(String image_thb, String id, String author, String imageUrl, String title){
+        this.image_thb = image_thb;
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImage_thb() { return this.image_thb; }
+
+    public String getAuthor() { return this.author; }
+
+    public String getTitle() { return this.title; }
+
+    public String getImageUrl() { return this.imageUrl; }
+
+    public String getId() { return this.id; }
+
+}
