3737454cd22466bb357d0cf678c9533268fb90a0
tommi.bonetti <tommi.bonetti@gmail.com>
2017-06-02 15:31:29 +0200

    Add Controller code

--- app/src/main/java/love/flicli/controller/Controller.java
+++ app/src/main/java/love/flicli/controller/Controller.java
@@ -1,5 +1,8 @@
 package love.flicli.controller;
 
+import android.content.Context;
+import android.support.annotation.UiThread;
+
 import love.flicli.MVC;
 
 /**
@@ -13,4 +16,51 @@ public class Controller {
     public void setMVC(MVC mvc) {
         this.mvc = mvc;
     }
+
+    @UiThread
+    public void flicker(Context context, String n) {
+        //passiamo all servizio la stringa della ricerca più il contensto
+        FlickerService.flicker(context, n);
+        showHistory();
+    }
+    @UiThread
+    public void recent(Context context) {
+        //passiamo al servizio il contensto
+        FlickerService.recent(context);
+        showHistory();
+    }
+
+    @UiThread
+    public void popular(Context context) {
+        //passiamo al servizio il contensto
+        FlickerService.popular(context);
+        showHistory();
+    }
+    @UiThread
+    public void comment(Context context, String image) {
+        FlickerService.comment(context, image);
+    }
+
+
+    @UiThread
+    public void getImageView(Context context, FlickerService.Flick image) {
+        mvc.model.storeLastFlick(image);
+        comment(context, image.getId());
+        showImage();
+    }
+
+    public void showAuthorLastImage(Context context, String author) {
+        FlickerService.lastAuthorImage(context, author);
+        showHistory();
+    }
+
+    @UiThread
+    public void showHistory() {
+        mvc.forEachView(View::showHistory);
+    }
+
+
+    @UiThread
+    public void showImage() { mvc.forEachView(View::showImage); }
+    
 }
