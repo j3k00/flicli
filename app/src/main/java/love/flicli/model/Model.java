c0619533724271be7ac16ecfee9b802cdd003fad
tommi.bonetti <tommi.bonetti@gmail.com>
2017-06-02 16:24:11 +0200

    merge model code

--- app/src/main/java/love/flicli/model/Model.java
+++ app/src/main/java/love/flicli/model/Model.java
@@ -21,8 +21,47 @@ public class Model {
     // I really need that info?
     public static final String SECRET_KEY = "10fde0314d4d1aef";
 
+    private MVC mvc;
+
+    @GuardedBy("Itself")
+    private FlickerService.Flick[] flickers;
+    private FlickerService.Flick lastFlick;
+    private FlickerService.Comments[] comments;
+
+    private final static int MAX_FLICK = 50;
+
     public void setMVC(MVC mvc) {
         this.mvc = mvc;
     }
 
+    public void storeFactorization(FlickerService.Flick[] f) {
+        flickers = f.clone();
+        mvc.forEachView(View::onModelChanged);
+    }
+
+    public void storeComments(FlickerService.Comments[] c) {
+        comments = c.clone();
+        mvc.forEachView(View::onModelChanged);
+    }
+
+    public void storeLastFlick(FlickerService.Flick flick) {
+        lastFlick = flick;
+    }
+
+    public FlickerService.Comments[] getComments() {
+        if (comments != null)
+            return comments.clone();
+        else
+            return new FlickerService.Comments[0];
+    }
+
+    public FlickerService.Flick getLastFlick() { return lastFlick; }
+
+    //return the list of the search Flick
+    public FlickerService.Flick[] getFlickers() {
+        if (flickers != null)
+            return flickers.clone();
+        else
+            return new FlickerService.Flick[0];
+    }
 }
