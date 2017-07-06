84489ff24a4e2c61aea35f611a76726ff95e0e97
tommi.bonetti <tommi.bonetti@gmail.com>
2017-07-06 22:45:34 +0200

    finally

--- app/src/main/java/love/flicli/view/DetailImageFragment.java
+++ app/src/main/java/love/flicli/view/DetailImageFragment.java
@@ -1,7 +1,10 @@
 package love.flicli.view;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -12,8 +15,10 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.appcompat.BuildConfig;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +31,7 @@ import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -35,6 +41,7 @@ import love.flicli.R;
 import love.flicli.model.FlickModel;
 
 import static android.R.attr.bitmap;
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 import static android.support.v4.content.FileProvider.getUriForFile;
 import static java.io.File.createTempFile;
@@ -44,19 +51,23 @@ import static java.io.File.createTempFile;
  */
 
 public class DetailImageFragment extends Fragment implements AbstractFragment {
+/*
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+*/
     private MVC mvc;
     ShareActionProvider mShareActionProvider = null;
     private FlickModel flickModel;
     private final static String TAG = DetailImageFragment.class.getName();
 
-
-    public DetailImageFragment() {
-        
-    }
     @Override @UiThread
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        //verifyStoragePermissions(getActivity());
     }
 
     @Override
@@ -93,24 +104,29 @@ public class DetailImageFragment extends Fragment implements AbstractFragment {
             shareIntent.setAction(Intent.ACTION_SEND);
             shareIntent.setType("image/jpeg");
 
-            PackageManager pm = ((FlicliApplication) getActivity().getApplication()).getPackageManager();
+            //File f = new File(Environment.getExternalStorageDirectory()+ File.separator + "temp.jpeg");
+            //File dir = new File(Environment.getExternalStorageDirectory()+ File.separator + "images");
+            //dir.mkdirs();
+            //File f = new File(dir, "temp.jpeg");
 
-            File f = new File(Environment.getDataDirectory() + File.separator + "temp.jpeg");
+            File imagePath = new File(getActivity().getApplication().getFilesDir(), "images");
+            File newFile = new File(imagePath, "default_image.jpg");
 
             try {
-
                 Bitmap bmp = mvc.model.getDetailFlicker().getBitmap_url_s();
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 bmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                f.createNewFile();
-                new FileOutputStream(f).write(stream.toByteArray());
+                imagePath.mkdir();
+                new FileOutputStream(newFile).write(stream.toByteArray());
 
             } catch (IOException e) {
                 e.getMessage();
             }
 
+            Uri contentUri = getUriForFile(getActivity().getApplication(), "love.flicli.fileprovider", newFile);
 
-            shareIntent.putExtra(Intent.EXTRA_STREAM, f.getPath());
+            shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+            shareIntent.setFlags(FLAG_GRANT_READ_URI_PERMISSION);
             myShareActionProvider.setShareIntent(shareIntent);
         }
     }
@@ -140,6 +156,21 @@ public class DetailImageFragment extends Fragment implements AbstractFragment {
         ft.commit();
     }
 
+/*
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+*/
 
     public void showAuthorLastImage(String author) {
         //mvc.controller.showAuthorLastImage(getActivity(), author);
