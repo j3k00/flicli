718568b572764ad859f91c533d1750a44e169219
tommi.bonetti <tommi.bonetti@gmail.com>
2017-08-17 21:34:27 +0200

    fix preview bugs

--- app/src/main/java/love/flicli/Util.java
+++ app/src/main/java/love/flicli/Util.java
@@ -21,6 +21,10 @@ public class Util {
 
         File newFile = new File(imagePath, "default_image.jpg");
 
+        //previene bug sulla visualizzazione della preview delle immagini
+        if (newFile.exists())
+            newFile.delete();
+
         try {
             Bitmap bmp = image;
             if (bmp != null) {
